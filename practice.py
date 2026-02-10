918d7951eedbff23bfd97df9dbccc9285f5cc3ad
Dmitrii-Brandt <brandtdmitrii@gmail.com>
2026-02-10 15:01:41 +0200

    Practiced some python

---
+++ practice.py
@@ -0,0 +1,44 @@
+# TASK ONE
+my_name = 'Dmitrii'
+my_age = 1000
+print(f'My name is  {my_name} and my age is {my_age}')
+print('My name is ', my_name, ' and my age is ', my_age)
+my_num = 100.232
+print(my_num * 2)
+# TASK TWO
+
+sentence = 'Just when i thought i was out, they pull me back in'
+print(sentence.upper())
+print(sentence.lower())
+print(len(sentence))
+print(sentence[-3::-1])
+
+# TASK THREE
+num_a = 17
+num_b = 23
+print('nums sum is', num_a + num_b)
+print('nums difference is', num_a - num_b)
+print('nums product is', num_a * num_b)
+print('nums division is', num_a / num_b)
+print('nums remainder of a division is', num_a % num_b)
+print('num_a in a power of 3 is', pow(num_a, 3))
+
+# TASK FOUR
+fruits = ['banana', 'watermelon', 'strawberry', 'apple', 'pineapple']
+print(fruits[0], fruits[-1])
+fruits.append('mango')
+print(fruits[0], fruits[-1])
+print(fruits)
+if len(fruits) % 2 != 0:
+    print('The middle fruit is ', fruits[int((len(fruits)- 1) / 2)])
+else:
+    print("There's no middle in the list")
+fruits.append('peach')
+print(fruits)
+if len(fruits) % 2 != 0:
+    print('The middle fruit is ', fruits[int((len(fruits)- 1) / 2)])
+else:
+    print("There's no middle in the list")
+#fruits.remove(fruits[round(len(fruits)/2,0)])
+#print(middle_fruit)
+#print(int(len(fruits) / 2))

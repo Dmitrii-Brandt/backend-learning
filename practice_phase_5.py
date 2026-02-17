5356907c00074dedc4f25314394ac1d5f9ff0710
Dmitrii-Brandt <brandtdmitrii@gmail.com>
2026-02-17 09:20:03 +0200

    done some venv practice and have met gitignore

--- practice_phase_5.py
+++ practice_phase_5.py
@@ -1,11 +1,21 @@
+# TASK 17 EX
+'''
 def dev_it():
-    user_num= int(input("Insert a number: "))
     try:
-        result = 100 / user_num
-    except ZeroDivisionError as zde:
-        print(zde)
-        print("Number isn't right")
-    else:
-        print(f"Your answer is {result}")
+        user_num= int(input("Insert a number: "))
+        
 
+    except ValueError as ve:
+        print(ve)
+        print("I said number...")
+
+    else:
+        try:
+            result = 100 / user_num
+        except ZeroDivisionError as zde:
+            print(zde)
+            print("Number isn't right")
+        else:
+            print(f"Your answer is {result}")
 dev_it()
+'''
